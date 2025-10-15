cce44555cdcc2297fba24894449f90fa9ee358e8
Dewarjuna <hehe31134@gmail.com>
2025-10-15 09:06:13 +0700

    feat(home): added home page
    - added home page after clicking url

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,21 @@
+import Navbar from "@/components/Navbar"
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Menu from "@/components/Menu";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Menu />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
